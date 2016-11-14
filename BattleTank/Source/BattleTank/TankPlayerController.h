8895467b1eb8a0c1f688acb9905199b92b11496c
SIXTO\Admin <6togomez@gmail.com>
2016-11-13 21:52:39 -0400

    GetLookDirection

--- BattleTank/Source/BattleTank/TankPlayerController.h
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -28,10 +28,14 @@ private:
 		// Return an OUT parameter, true if hit landscape
 		bool GetSightRayHitLocation(FVector &HitLocation) const;
 
+		bool GetLookDirection(FVector2D ScreenLocation, FVector & LookDirection) const;
+
 		UPROPERTY( EditAnywhere )
 		float CrossHairXLocation = 0.5;
 
 		UPROPERTY( EditAnywhere )
 		float CrossHairYLocation = 0.33333;
 
+
+		
 };
