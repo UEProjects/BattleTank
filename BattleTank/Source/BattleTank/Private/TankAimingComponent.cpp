f584653ce81571b6daf39cf911c50945152a92df
SIXTO\Admin <6togomez@gmail.com>
2016-11-16 20:08:08 -0400

    Review Our Execution Flow

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -11,7 +11,7 @@ UTankAimingComponent::UTankAimingComponent()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	bWantsBeginPlay = true;
-	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.bCanEverTick = true;  //TODO should this really Tick?
 
 	// ...
 }
@@ -39,13 +39,19 @@ void UTankAimingComponent::AimAt(FVector HitLocation, float LaunchSpeed)
 		    LaunchSpeed,  
 		    ESuggestProjVelocityTraceOption::DoNotTrace   // Calculate the OutLaunchVelocity
 		);
+
 		if(bHaveAimSolution)
-		{
+	    {
 		auto AimDirection = OutLaunchVelocity.GetSafeNormal();
-		auto TankName = GetOwner()->GetName();
 		MoveBarrelTowards(AimDirection);
-		// UE_LOG(LogTemp, Warning, TEXT("%s Aiming at %s"), *TankName, *AimDirection.ToString());
-	}
+		auto Time = GetWorld()->GetTimeSeconds();
+		UE_LOG(LogTemp, Warning, TEXT("%f: Aim solution found"), Time);
+	    }
+		else
+		{
+			auto Time = GetWorld()->GetTimeSeconds();
+			UE_LOG(LogTemp, Warning, TEXT("%f No Aim solve found"), Time);
+		}
 	// If no solution found do nothing
 }
 
@@ -56,7 +62,7 @@ void UTankAimingComponent::MoveBarrelTowards(FVector AimDirection)
 	auto BarrelRotator = Barrel->GetForwardVector().Rotation();
 	auto AimAsRotator = AimDirection.Rotation();
 	auto DeltaRotator = AimAsRotator - BarrelRotator;
-	UE_LOG(LogTemp, Warning, TEXT("AimAsRotator: %s"), *DeltaRotator.ToString());
+	
 	
 	Barrel->Elevate(5);  //TODO remove magic number
 
