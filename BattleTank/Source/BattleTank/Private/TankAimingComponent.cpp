f58a50c28fe783367c9ba17be42783a5212da3ef
SIXTO\Admin <6togomez@gmail.com>
2016-11-22 16:28:48 -0400

    Firing Method and Projectile Class

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -56,28 +56,8 @@ void UTankAimingComponent::AimAt(FVector HitLocation, float LaunchSpeed)
 	    {
 		auto AimDirection = OutLaunchVelocity.GetSafeNormal();
 		MoveBarrelTowards(AimDirection);
-		auto Time = GetWorld()->GetTimeSeconds();
-		UE_LOG(LogTemp, Warning, TEXT("%f: Aim solution found"), Time);
 	    }
-		else
-		{
-			auto Time = GetWorld()->GetTimeSeconds();
-			UE_LOG(LogTemp, Warning, TEXT("%f No Aim solve found"), Time);
-		}
-
-		if(bHaveAimSolution)
-	    {
-		auto AimDirection = OutLaunchVelocity.GetSafeNormal();
-		MoveBarrelTowards(AimDirection);
-		auto Time = GetWorld()->GetTimeSeconds();
-		UE_LOG(LogTemp, Warning, TEXT("%f: Aim solution found"), Time);
-	    }
-		else
-		{
-			auto Time = GetWorld()->GetTimeSeconds();
-			UE_LOG(LogTemp, Warning, TEXT("%f No Aim solve found"), Time);
-		}
-	// If no solution found do nothing
+	    // If no solution found do nothing
 }
 
 void UTankAimingComponent::MoveBarrelTowards(FVector AimDirection)
