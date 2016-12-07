408d990608a5a456077f58f591ba7925905d4f75
SIXTO\Admin <6togomez@gmail.com>
2016-12-06 22:37:14 -0400

    A better Component Architecture - Completing Manual Tank

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -8,7 +8,6 @@
 
 void UTankTrack::SetThrottle(float Throttle)
 {
-	//TODO Clamp actual Throttle value
 	auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
 	auto ForceLocation = GetComponentLocation();
 	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
